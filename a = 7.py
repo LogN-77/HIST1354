83f627b5706aef4ed6d6b8813bafc27154671aee
LogN-77 <68696808+LogN-77@users.noreply.github.com>
2024-10-24 09:49:49 -0500

    Sep 12 Class Exercise

---
+++ a = 7.py
@@ -0,0 +1,10 @@
+a = 7
+b = 3
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
+if a > b:
+    print ("a is bigger than b")
+else:
+    print ("a is not bigger than b")
---
+++ fruits.py
@@ -0,0 +1,6 @@
+fruits = ["watermelon", "raspberry", "banana", "apple", "pear"]
+for fruit in fruits:
+    if fruit == "watermelon":
+        print(fruit, "- I love it")
+    else: 
+        print(fruit)
